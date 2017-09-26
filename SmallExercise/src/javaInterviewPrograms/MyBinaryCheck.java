6063e65b65cb7e6d436d04441494a99dad0a7b9b
Helen Zhu <zhu22c@mtholyoke.edu>
2017-09-25 22:41:12 -0400

    more programs

---
+++ SmallExercise/src/javaInterviewPrograms/MyBinaryCheck.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package javaInterviewPrograms;
+
+/**
+ * @author Ellen
+ *
+ */
+public class MyBinaryCheck {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
