06e31c6e32659a7cab2c72e5a7857874a72a7582
LM\n0271239 <caiyun.zhu@libertymutual.com>
2017-12-12 13:21:54 -0500

    add

--- SmallExercise/src/javaInterviewPrograms/DecimalToBinary.java
+++ SmallExercise/src/javaInterviewPrograms/DecimalToBinary.java
@@ -8,7 +8,7 @@ public class DecimalToBinary {
 
     public void printBinaryFormat(int number){
 
-        int binary[] = new int[25];
+        int binary[] = new int[number];
         int index = 0;
         while(number > 0){
             binary[index++] = number%2;
@@ -22,6 +22,6 @@ public class DecimalToBinary {
 
     public static void main(String a[]){
         DecimalToBinary dtb = new DecimalToBinary();
-        dtb.printBinaryFormat(25);
+        dtb.printBinaryFormat(26);
     }
 }
